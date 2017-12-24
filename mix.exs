ecab298509ef55d39f637f97d37e88e050879060
Joseph An <an.joseph891@gmail.com>
2017-12-24 06:17:07 -0500

    Add ExAws dependencies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,18 @@ defmodule ExAws.Polly.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:hackney, ">= 0.0.0", only: [:dev, :test]},
+      {:sweet_xml, ">= 0.0.0", only: [:dev, :test]},
+      {:bypass, "~> 0.7", only: :test},
+      ex_aws(),
     ]
+  end
+
+  defp ex_aws() do
+    case System.get_env("AWS") do
+      "LOCAL" -> {:ex_aws, path: "../ex_aws"}
+      _ -> {:ex_aws, "~> 2.0.0"}
+    end
   end
 end
