81bdc28e7a72540118439a70b1726eb482076db7
Jon Zimbel <jzimbel@gmail.com>
2020-05-27 10:47:09 -0400

    Support all SynthesizeSpeech options except speech marks

--- lib/ex_aws/polly.ex
+++ lib/ex_aws/polly.ex
@@ -32,8 +32,12 @@ defmodule ExAws.Polly do
   ## Options
     * `:output_format` - The format in which the returned output will be encoded (mp3, ogg_vorbis, or pcm). Default is mp3.
     * `:voice_id` - Voice ID to use for the synthesis. You can get a list of available voice IDs by calling `ExAws.Polly.describe_voices`. Default is "Joanna".
+    * `:engine` - Specifies the engine (`standard` or `neural`) to use when processing input text for speech synthesis.
+    * `:language_code` - Optional language code for the Synthesize Speech request. This is only necessary if using a bilingual voice, such as Aditi, which can be used for either Indian English (en-IN) or Hindi (hi-IN).
+    * `:lexicon_names` - List of one or more pronunciation lexicon names you want the service to apply during synthesis.
     * `:sample_rate` - The audio frequency specified in Hz.  Valid values for mp3 and ogg_vorbis are "8000", "16000", and "22050". Valid values for pcm are "8000" and "16000". Default is "16000".
-  
+    * `:text_type` - Specifies whether the input text is plain text or SSML. Default is plain text.
+
   ## Example
 
       iex> ExAws.Polly.synthesize_speech("hello world") |> ExAws.request()
@@ -48,21 +52,29 @@ defmodule ExAws.Polly do
           {"Date", "Mon, 25 Dec 2017 01:23:42 GMT"}], status_code: 200}}
   """
   def synthesize_speech(text, opts \\ []) do
-    output_format = Keyword.get(opts, :output_format, "mp3")
-    voice_id = Keyword.get(opts, :voice_id, "Joanna")
-    sample_rate = Keyword.get(opts, :sample_rate, "16000")
+    {output_format, opts} = Keyword.pop(opts, :output_format, "mp3")
+    {voice_id, opts} = Keyword.pop(opts, :voice_id, "Joanna")
 
-    body = %{
+    required_params = %{
       "Text" => text,
       "OutputFormat" => output_format,
-      "VoiceId" => voice_id,
-      "SampleRate" => sample_rate
+      "VoiceId" => voice_id
     }
+
+    optional_params = Enum.map(opts, fn {k, v} -> {format_opt(k), v} end) |> Enum.into(%{})
+
+    body = Map.merge(required_params, optional_params)
 
     request(:post, :synthesize_speech, body: body, path: "/v1/speech")
   end
 
-  defp request(http_method, action, opts \\ []) do
+  defp format_opt(:engine), do: "Engine"
+  defp format_opt(:language_code), do: "LanguageCode"
+  defp format_opt(:lexicon_names), do: "LexiconNames"
+  defp format_opt(:sample_rate), do: "SampleRate"
+  defp format_opt(:text_type), do: "TextType"
+
+  defp request(http_method, action, opts) do
     path = Keyword.get(opts, :path, "/")
     body = Keyword.get(opts, :body, "")
 
